2817406ef66a313eff94debcf3aeed2a146eede7
Mad-H96 <madawa96sl123@gmail.com>
2024-02-13 21:28:39 +0530

    2024213

--- react_app/src/components/Serveys.jsx
+++ react_app/src/components/Serveys.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import  './serveys.css'
+import { userStateContext } from '../contex/ContecProvider';
 
 const Serveys = () => {
+
+  const {surveys} = userStateContext();
+  console.log(surveys);
+
   return (
     <div className="main">
     <h1 className='heading'>Surveys</h1>
